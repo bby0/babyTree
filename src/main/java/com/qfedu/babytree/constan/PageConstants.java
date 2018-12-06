e76af09b809caf44b9e80f45730b978641bd3bfd
blingbling <gssblingbling@163.com>
2018-12-06 21:32:37 +0800

    啊

---
+++ src/main/java/com/qfedu/babytree/constan/PageConstants.java
@@ -0,0 +1,12 @@
+package com.qfedu.babytree.constan;
+
+/**
+ * 常量类，保存常量
+ * @author Administrator
+ *
+ */
+public class PageConstants {
+	public static final int PAGESIZE = 5;
+	public static final int PAGENUM = 1;
+	
+}
