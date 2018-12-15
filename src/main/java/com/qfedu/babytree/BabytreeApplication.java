a3907b970e23a4ac62dd2bdcea0739da2f5c7f3c
bby0 <nuanbaobao123@gmail.com>
2018-12-15 09:04:51 +0800

    啊~朋友再见吧~

--- src/main/java/com/qfedu/babytree/BabytreeApplication.java
+++ src/main/java/com/qfedu/babytree/BabytreeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 /**
- * 著作人：第七组（育宝宝）---》彭奕茗，567，BlingBling,炎彬，寒冰，东伟，润华
+ * 著作人：第七组（育宝宝）---》彭奕茗，567，BlingBling,王炎彬，刘寒冰，何东伟，董润华
  */
 @SpringBootApplication
 @MapperScan(value = "com.qfedu.babytree.mapper")
